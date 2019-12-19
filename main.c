6f499e29bdea3d981ad874887374d85463077aaa
Mauricio Chávez <mauriciochavezgt@gmail.com>
2019-12-19 14:33:00 -0600

    Ejemplo 1

---
+++ main.c
@@ -0,0 +1,12 @@
+//Ejemplo 1
+#include <stdio.h>
+//#include <stdlib.h>
+#include <conio.h>
+int main()
+{
+    int x=7,y=2 ;
+    printf("%d", x%y);
+    getch();
+    //printf("Hello world!\n");
+    return 0;
+}
