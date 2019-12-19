e4b5a36304c1f314fc48b386a67c26e28b2e6904
Mauricio Chávez <mauriciochavezgt@gmail.com>
2019-12-19 14:37:50 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-//Ejemplo 1
+//Ejemplo incremento
 #include <stdio.h>
 //#include <stdlib.h>
 #include <conio.h>
 int main()
 {
-    int x=7,y=2 ;
-    printf("%d", x%y);
+    int a=7,b;
+    b= ++a+3; // a se incremente en 1, le sumamos 3 y b recibe 11
+    b= b+a+++5; // b recibe 11+8+5 y la a se incrementa
+    //printf("%d,%d" , a,b);
+    printf("%d,%d" , a,b);
     getch();
     //printf("Hello world!\n");
     return 0;
